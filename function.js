915aac160963cb33229e5b0a95434d94ca9d403c
salmoon231 <salmoonsamuel08@gmail.com>
2021-02-02 02:01:06 -0500

    initiate

---
+++ function.js
@@ -0,0 +1,12 @@
+alert("Hello, Welcome to Ss Foods")
+
+var btn = document.getElementById("submit");
+btn.addEventListener("click", submit);
+function zipcode(number){
+    document.getElementsByClassName("zipcode")
+    if(zipcode > 2000){
+        return true;
+    }else {
+        return false;
+    }
+};
