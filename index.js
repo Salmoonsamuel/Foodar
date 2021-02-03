8546c7429120ef94d0c68b8f72635df176cd6a24
salmoon231 <salmoonsamuel08@gmail.com>
2021-02-03 16:07:14 -0500

    updated Js file
    Fix function codes.

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("Hello! Everybody! Welcome to Ss Foods");
+
+function register(){
+  var zip1 =document.getElementById("zip").Value;
+ 
+ document.getElementById("message").innerHTML =  " Congrats! You 're in Service Area!";
+}
--- script.js
+++
@@ -1,7 +0,0 @@
-
-alert("Hello! Welcome to Salmoon's Portfolio Website. Thanks for visiting.")
-// function register(){
-//     var zip =
-//     document.getElementById("zipc").Value;
-//     document.getElementById("message").innerHTML = zip + ", You 're in Service Area!";
-// };
