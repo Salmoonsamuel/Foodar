a082dcda24496c0d87c8176b68c94d240d9a7a36
salmoon231 <salmoonsamuel08@gmail.com>
2021-02-03 15:16:47 -0500

    update
    I update some functionality code.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+alert("Hello! Welcome to Salmoon's Portfolio Website. Thanks for visiting.")
+// function register(){
+//     var zip =
+//     document.getElementById("zipc").Value;
+//     document.getElementById("message").innerHTML = zip + ", You 're in Service Area!";
+// };
